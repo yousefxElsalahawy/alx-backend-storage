c153671d39b26dd5c245d3bad9eb356dd0cb2cda
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-16 23:54:00 +0300

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -11,11 +11,9 @@ def print_nginx_request_logs(nginx_collection):
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
+        req_count = nginx_collection.count_documents({'method': method})
         print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
+    status_checks_count = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
     print('{} status check'.format(status_checks_count))
 
 
